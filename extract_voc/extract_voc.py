30789c143efcb3da4c8ed8098181102f99bbae0f
derekmma <hi@derek.ma>
2017-11-26 15:30:09 +0800

    finish extract_voc, get chatbot vocabulary

---
+++ extract_voc/extract_voc.py
@@ -0,0 +1,20 @@
+import sys
+import re
+import os
+from bs4 import BeautifulSoup
+
+filename = 'voc.html'
+doc = open(filename).read()
+
+soup = BeautifulSoup(doc)
+table = soup.find("table", attrs={"class":"details"})
+
+text = ''
+for row in table.find_all("tr"):
+    for td in row.find_all("td"):
+        text += td.get_text() + '\n'
+text = text[:-1]
+
+text_file = open("../voc.txt", "w")
+text_file.write(text)
+text_file.close()
